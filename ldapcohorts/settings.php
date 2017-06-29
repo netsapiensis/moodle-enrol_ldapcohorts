0b703782a1ed064f1838cdbd9f815f64805b055b
faty <andrei.fatiol@yahoo.com>
2017-06-29 20:30:31 +0300

    upgrade to moodle 3.2

--- ldapcohorts/settings.php
+++ ldapcohorts/settings.php
@@ -36,7 +36,7 @@ if ($ADMIN->fulltree) {
         $settings->add(new admin_setting_heading('enrol_ldap_cohort_server_settings', get_string('server_settings', 'enrol_ldapcohorts'), ''));
         $settings->add(new admin_setting_configtext('enrol_ldapcohorts/host_url', get_string('host_url_key', 'enrol_ldapcohorts'), get_string('host_url', 'enrol_ldapcohorts'), ''));
         $settings->add(new admin_setting_configselect('enrol_ldap/start_tls', get_string('start_tls_key', 'auth_ldap'), get_string('start_tls', 'auth_ldap'), 0, $yesno));
-        $settings->add(new admin_setting_configtext_trim_lower('enrol_ldap/ldapencoding', get_string('ldap_encoding_key', 'enrol_ldap'), get_string('ldap_encoding', 'enrol_ldap'), 'utf-8'));
+        $settings->add(new admin_setting_ldapcohort_trim_lower('enrol_ldap/ldapencoding', get_string('ldap_encoding_key', 'enrol_ldap'), get_string('ldap_encoding', 'enrol_ldap'), 'utf-8'));
         // Set LDAPv3 as the default. Nowadays all the servers support it and it gives us some real benefits.
         $options = array(3=>'3', 2=>'2');
         $settings->add(new admin_setting_configselect('enrol_ldapcohorts/ldap_version', get_string('version_key', 'enrol_ldapcohorts'), get_string('version', 'enrol_ldapcohorts'), 3, $options));
@@ -44,7 +44,7 @@ if ($ADMIN->fulltree) {
         
         //--- bind settings
         $settings->add(new admin_setting_heading('enrol_ldapcohorts_bind_settings', get_string('bind_settings', 'enrol_ldapcohorts'), ''));
-        $settings->add(new admin_setting_configtext_trim_lower('enrol_ldapcohorts/bind_dn', get_string('bind_dn_key', 'enrol_ldapcohorts'), get_string('bind_dn', 'enrol_ldapcohorts'), ''));
+        $settings->add(new admin_setting_ldapcohort_trim_lower('enrol_ldapcohorts/bind_dn', get_string('bind_dn_key', 'enrol_ldapcohorts'), get_string('bind_dn', 'enrol_ldapcohorts'), ''));
         $settings->add(new admin_setting_configpasswordunmask('enrol_ldapcohorts/bind_pw', get_string('bind_pw_key', 'enrol_ldapcohorts'), get_string('bind_pw', 'enrol_ldapcohorts'), ''));
         
         //--- cohort lookup settings
--- ldapcohorts/version.php
+++ ldapcohorts/version.php
@@ -1,4 +1,8 @@
 <?php
+defined('MOODLE_INTERNAL') || die();
 
+$plugin->version  = 2017062600;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = 'LDAP Cohort Synchronization';
+$plugin->component = 'enrol_ldapcohorts';
 $plugin->cron = 180;
-$plugin->version = 2014080100;
