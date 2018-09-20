6c556c88409002f5e91acb571b19a8fab3cd6db0
netsapiensis <andrei.fatiol@yahoo.com>
2018-09-20 23:41:07 +0300

    Moodle 3.5 support

--- ldapcohorts/version.php
+++ ldapcohorts/version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2017071302;
+$plugin->version  = 2018092000;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'LDAP Cohort Synchronization';
 $plugin->component = 'enrol_ldapcohorts';
